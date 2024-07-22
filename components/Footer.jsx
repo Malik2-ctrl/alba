59c54877ab2b839c2aa8da31e5fb4aa6a585dcf8
Malik2-ctrl <nexthustles21@gmail.com>
2024-07-22 23:38:00 +0200

    Update Footer.jsx

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -3,7 +3,7 @@ export default function Footer() {
     <div className="sm:fixed text-xs sm:text-sm bottom-0 w-screen font-satoshithin px-4 mb-2 pr-6 flex justify-between">
       <div>Alba Preysing</div>
       <div>London, England</div>
-      <div>@albaelena</div>
+      <div>@albaaelena</div>
     </div>
   );
 }
